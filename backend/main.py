374d1494bd570b094703bc55e6ee6a3567d8000e
Ian <kotonefemc@gmail.com>
2023-11-14 14:44:02 +0000

    journaling functionality working

--- backend/main.py
+++ backend/main.py
@@ -2,48 +2,67 @@ import os
 import sys
 import datetime
 import logging
-import difflib
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+def line_diff(old_lines, new_lines):
+        old_line_number = 1
+        new_line_number = 1
+        changes = ''
+        while old_line_number <= len(old_lines) or new_line_number <= len(new_lines):
+            if old_line_number > len(old_lines):
+                changes += f'{datetime.datetime.now()}\t\t\t+\t\t\tl{new_line_number}:{new_lines[new_line_number-1]}'
+                new_line_number += 1
+            elif new_line_number > len(new_lines):
+                changes += f'{datetime.datetime.now()}\t\t\t-\t\t\tl{old_line_number}:{old_lines[old_line_number-1]}'
+                old_line_number += 1
+            elif old_lines[old_line_number-1] != new_lines[new_line_number-1]:
+                changes += f'{datetime.datetime.now()}\t\t\t-\t\t\tl{old_line_number}:{old_lines[old_line_number-1]}'
+                changes += f'{datetime.datetime.now()}\t\t\t+\t\t\tl{new_line_number}:{new_lines[new_line_number-1]}'
+                old_line_number += 1
+                new_line_number += 1
+            else:
+                old_line_number += 1
+                new_line_number += 1
+        return changes
 
 class FileChangeHandler (FileSystemEventHandler):
     def __init__(self):
         self.last_seen = {}
 
     def on_modified(self, event):
-        if event.src_path.split(".")[-1] == "dat":
-            return
-        filename: str = event.src_path
-        dat_filename = '../watched_folder/' + filename.split('/')[-1] + '.dat'
         try:
-            with open(filename, 'r') as f:
-                lines = f.readlines()
-        except FileNotFoundError:
-            print('File not found')
-            return
-        except IsADirectoryError:  
-            print('Is a directory')
-            return
+            if event.src_path.split(".")[-1] == "dat" or not os.path.isfile(event.src_path):
+                return
+            filename: str = event.src_path
+            print(filename)
+            dat_filename = '../watched_folder/' + filename.split('/')[-1] + '.dat'
+            try:
+                with open(filename, 'r') as f:
+                    lines = f.readlines()
+            except FileNotFoundError:
+                print('File not found')
+                return
+            except IsADirectoryError:  
+                print('Is a directory')
+                return
 
-        if filename in self.last_seen:
-            old_lines = self.last_seen[filename]
-            old_line_num = 1
-            new_line_num = 1
-            for line in difflib.ndiff(old_lines, lines):
-                if line.startswith('- '):
-                    change = f'{datetime.datetime.now()}\t\t\t-\t\t\tl{old_line_num}: {line[2:]}\n'
-                    old_line_num += 1
-                elif line.startswith('+ '):
-                    change = f'{datetime.datetime.now()}\t\t\t+\t\t\tl{new_line_num}: {line[2:]}\n'
-                    new_line_num += 1
-                else:
-                    old_line_num += 1
-                    new_line_num += 1
-                with open(dat_filename, 'a') as f:
-                    f.write(change)
+            if filename in self.last_seen:
+                old_lines = self.last_seen[filename]
+                changes = line_diff(old_lines, lines)
+                if changes:
+                    try:
+                        with open(dat_filename, 'a') as f:
+                            f.write(changes)
+                    except:
+                        print('Error writing to file')
+                        pass
 
-        self.last_seen[filename] = lines
+            self.last_seen[filename] = lines
+        except Exception as e:
+            print(e)
+            pass
 
     def on_created(self, event):
         if event.src_path.split(".")[-1] == "dat":
@@ -62,8 +81,10 @@ class FileChangeHandler (FileSystemEventHandler):
         # Create the .dat file
         with open(dat_filename, 'w') as f:
             f.write("Time Stamp\t\t\t\t\t\t\ta/r\t\t\tline\n")
+            linenum = 1
             for line in lines:
-                f.write(f'{datetime.datetime.now()}\t\t\t+\t\t\t{line}')
+                f.write(f'{datetime.datetime.now()}\t\t\t+\t\t\tl{linenum}:{line}')
+                linenum += 1
         
     def on_deleted(self, event):
         if event.src_path.split(".")[-1] == "dat":
@@ -73,9 +94,11 @@ class FileChangeHandler (FileSystemEventHandler):
         
         if filename in self.last_seen:
             lines = self.last_seen[filename]
+            linenum = 1
             with open(dat_filename, 'a') as f:
                 for line in lines:
-                    f.write(f'{datetime.datetime.now()}\t\t\tr\t\t\t{line}')
+                    f.write(f'{datetime.datetime.now()}\t\t\t-\t\t\tl{linenum}:{line}')
+                    linenum += 1
         else:
             print('File not found in last_seen')
 
