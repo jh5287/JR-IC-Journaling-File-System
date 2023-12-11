5a25d06e41fa9ce5c5533913bce67134679610ff
Ian <kotonefemc@gmail.com>
2023-12-11 19:52:21 +0000

    added user input

--- backend/replay.py
+++ backend/replay.py
@@ -22,4 +22,14 @@ def replay(filename, line_count):
         for line_number in sorted(file_state.keys()):
             f.write(file_state[line_number])
 
-replay('../watched_folder/new.txt', 11)
+print('Choose the file path of the file you\'d like to replay to: ../watched_foler/')
+file_path = input()
+with open(f'../watched_folder/{file_path}.dat', 'r') as f:
+    lines = f.readlines()
+    for i, line in enumerate(lines):
+        if i == 0:
+            continue
+        print(f'{i}:{line}', end='')
+print("Choose the line you'd like to replay to: ")
+line_number = int(input())
+replay('../watched_folder/new.txt', line_number)
